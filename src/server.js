21a6e8cbe528e36a443ce4374a3a3922af34c393
Ivan Barbosa <ivanbarbosag@gmail.com>
2023-02-24 17:26:16 -0300

    Create Database

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
+const database = require("./database/sqlite");
 const routes = require("./routes");
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running ${PORT}`));
