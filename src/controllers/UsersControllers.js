88e1ec9a510c9115b58e973c4d6c2d4848b65e41
Ivan Barbosa <ivanbarbosag@gmail.com>
2023-02-25 15:33:26 -0300

    Config update user

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -9,7 +9,6 @@ class UsersController {
         const database = await sqliteConnection();
         const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-
         if(checkUserExists) {
             throw new AppError("Este e-mail ja está em uso.");
         }
@@ -18,11 +17,40 @@ class UsersController {
             "INSERT INTO users (name, email, password, is_admin) VALUES (?, ?, ?, ?)",
             [name, email, password, is_admin]
         );
-
         
         return response.status(201).json({ name, email, password, is_admin });
     }
 
+    async update(request, response) {
+        const { name, email } = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if(!user) {
+            throw new AppError("Usuário não encontrado")
+        }
+
+        const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+            throw new AppError("Este e-mail já está em uso.");
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(`UPDATE users SET
+        name = ?,
+        email = ?,
+        updated_at = ?
+        WHERE id = ?`,
+        [user.name, user.email, new Date(), id]
+        );
+
+        return response.json();
+    }
     
 }
 
