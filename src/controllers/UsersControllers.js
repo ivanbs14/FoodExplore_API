8ca776e969e0a3528bb6cf8e047e11179f0eddd4
Ivan Barbosa <ivanbarbosag@gmail.com>
2023-02-25 14:21:44 -0300

    Config create new user

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -1,10 +1,27 @@
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-    create(request, response) {
+    /* create and insert and database data from users */
+    async create(request, response) {
         const { name, email, password, is_admin } = request.body;
 
-        response.json({ name, email, password, is_admin });
+        const database = await sqliteConnection();
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+
+        if(checkUserExists) {
+            return response.status(401).json();
+        }
+
+        await database.run(
+            "INSERT INTO users (name, email, password, is_admin) VALUES (?, ?, ?, ?)",
+            [name, email, password, is_admin]
+        );
+
+        return response.status(201).json();
     }
+
+    
 }
 
 module.exports = UsersController;
