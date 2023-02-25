e56c9505802f009746ab6d0f7277dc05316dceb8
Ivan Barbosa <ivanbarbosag@gmail.com>
2023-02-25 17:16:06 -0300

    Config validation password and errors

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const sqliteConnection = require("../database/sqlite");
 const AppError = require("../utils/AppError");
 
@@ -18,14 +18,14 @@ class UsersController {
 
         await database.run(
             "INSERT INTO users (name, email, password, is_admin) VALUES (?, ?, ?, ?)",
-            [name, email, hashedPassword, is_admin]
+            [name, email, hashedPassword, Boolean(is_admin) ?? false]
         );
         
         return response.status(201).json({ name, email, password, is_admin });
     }
 
     async update(request, response) {
-        const { name, email } = request.body;
+        const { name, email, password, old_password } = request.body;
         const { id } = request.params;
 
         const database = await sqliteConnection();
@@ -41,15 +41,33 @@ class UsersController {
             throw new AppError("Este e-mail já está em uso.");
         }
 
-        user.name = name;
-        user.email = email;
+        
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+        if (password && !old_password) {
+            throw new AppError("Você precisa informar a senha antiga");
+        }
+
+        if (password && old_password) {
+            const checkUserPassword = await compare(old_password, user.password);
+
+            if (!checkUserPassword) {
+                throw new AppError("A senha antiga não confere.");
+            }
+
+            user.password = await hash(password, 8);
+        }
+
+
 
         await database.run(`UPDATE users SET
         name = ?,
         email = ?,
-        updated_at = ?
+        password = ?,
+        updated_at = DATETIME('now')
         WHERE id = ?`,
-        [user.name, user.email, new Date(), id]
+        [user.name, user.email, user.password, id]
         );
 
         return response.json();
