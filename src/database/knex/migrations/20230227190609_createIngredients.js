0acdc82c35945d21b051ad1997f6c149c80702c1
Ivan Barbosa <ivanbarbosag@gmail.com>
2023-02-27 16:13:48 -0300

    Adjust error delete dish

--- src/database/knex/migrations/20230227190609_createIngredients.js
+++ src/database/knex/migrations/20230227190609_createIngredients.js
@@ -2,7 +2,7 @@ exports.up = knex => knex.schema.createTable("ingredients", table => {
     table.increments("id");
     table.text("name").notNullable;
     
-    table.integer("dish_id").references("id").inTable("dish");
+    table.integer("dish_id").references("id").inTable("dish").onDelete("CASCADE");
     table.integer("user_id").references("id").inTable("users");
 
     
