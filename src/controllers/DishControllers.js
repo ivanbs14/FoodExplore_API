30d6009751eab3c371eebbb31414a8cc519e403d
Ivan Barbosa <ivanbarbosag@gmail.com>
2023-03-06 10:55:21 -0300

    Confing Filters

--- src/controllers/DishControllers.js
+++ src/controllers/DishControllers.js
@@ -6,6 +6,7 @@ class DishController {
         const { user_id } = request.params;
 
         const dish_id = await knex("dish").insert({
+            user_id,
             title,
             category,
             price,
@@ -44,6 +45,44 @@ class DishController {
         await knex("dish").where({ id }).delete("PRAGMA foreign_keys = 1");
 
         return response.json();
+    }
+
+    async filters(request, response) {
+        const { title, user_id, ingredients } = request.query;
+
+        let dish;
+
+        if(ingredients) {
+            const filterIngredients = ingredients.split(',').map(ingredients => ingredients.trim());
+
+            dish = await knex("ingredients")
+            .select([
+                "dish.id",
+                "dish.title",
+                "dish.user_id",
+            ])
+            .where("dish.user_id", user_id)
+            .whereLike("dish.title", `%${title}%`)
+            .whereIn("name", filterIngredients)
+            .innerJoin("dish", "dish.id", "ingredients.dish_id")
+            .orderBy("dish.title")
+
+        } else {
+            dish = await knex("dish").where({ user_id }).whereLike("title", `%${title}%`).orderBy("title");
+        }
+
+        const userIngredients = await knex("ingredients").where({ user_id });
+        const dishsIngredients = dish.map(dish => {
+            const dishIngredient = userIngredients.filter(ingredient => ingredient.dish_id === dish.id);
+
+            return {
+                ...dish,
+                ingredients: dishIngredient
+            }
+        });
+
+
+        return response.json(dishsIngredients);
     }
 }
 
--- src/routes/dish.routes.js
+++ src/routes/dish.routes.js
@@ -4,6 +4,7 @@ const DishController = require("../controllers/DishControllers");
 const dishRoutes = Router();
 const dishController = new DishController();
 
+dishRoutes.get("/", dishController.filters);
 dishRoutes.post("/:user_id", dishController.create);
 dishRoutes.get("/:id", dishController.previewDish);
 dishRoutes.delete("/:id", dishController.delete);
