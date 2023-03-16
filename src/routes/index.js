df72096c12be181c5893b9708d5a6e3e3f5569d4
Ivan Barbosa <ivanbarbosag@gmail.com>
2023-03-16 10:05:10 -0300

    Checking user in database

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require("express");
 const usersRouter = require("./users.routes");
 const dishRouter = require("./dish.routes");
 const ingredientsRouter = require("./ingredients.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 routes.use("/dish", dishRouter);
 routes.use("/ingredients", ingredientsRouter);
 
