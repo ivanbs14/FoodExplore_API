2efc4274177ff169da0f71fa7a269d489d350689
Ivan Barbosa <ivanbarbosag@gmail.com>
2023-06-30 22:49:44 -0300

    Adjustment controller DishSearch

--- src/controllers/DishSearchControllers.js
+++ src/controllers/DishSearchControllers.js
@@ -5,6 +5,9 @@ class DishSearchController {
     async dishSearch(request, response) {
         const { search } = request.query;
 
+        const user_id = request.user.id;
+        /* const user_id = 1; */
+
         let findByIngred;
         let findBydish;
         let dishWithIngredTwo = [];
@@ -19,12 +22,14 @@ class DishSearchController {
                 "dish.title",
                 "dish.price",
                 "dish.description",
+                "dish.img_dish",
             ])
+            .whereLike("ingredients.name", `%${search}%`)
             .whereIn("name", filterItem)
             .innerJoin("dish", "dish.id", "ingredients.dish_id")
             .orderBy("title");
 
-        const allItens = await knex("ingredients").whereLike("ingredients.name", `%${search}%`)
+        const allItens = await knex("ingredients").where({ user_id })
         const dishWithIngred = findByIngred.map(dis => {
             const itensWithDish = allItens.filter(itm => itm.dish_id === dis.id);
 
@@ -35,10 +40,10 @@ class DishSearchController {
         })
 
         /* query to display dish data */
-        findBydish = await knex("dish").whereLike("dish.title", `%${search}%`).orderBy("title")
+        findBydish = await knex("dish").whereLike("title", `%${search}%`).orderBy("title")
         const idDish = await findBydish[0]?.id;
         if(idDish){
-            const allItensTwo = await knex("ingredients").where({dish_id: idDish})
+            const allItensTwo = await knex("ingredients").where({ user_id })
             dishWithIngredTwo = findBydish.map(dish => {
                 const itensWithDishTwo = allItensTwo.filter(itens => itens.dish_id === dish.id);
 
